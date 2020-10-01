cb3bb1dbb8441b6658d89207e89ea0b6da9cbcb3
Enrique Lopez <elop22@gmail.com>
2020-09-30 19:19:48 -0600

    adding ajax call

--- public/js/api.js
+++ public/js/api.js
@@ -1,1 +1,20 @@
-// $(document).ready(() => {});
+const apiKey = "";
+const topBooks = [];
+
+$(document).ready(function(e){
+  $("#get-button").on("click" ,findBooks(), function(){
+    
+    e.preventDefault()
+  });
+  function findBooks() {
+    $.ajax({
+      method: "GET",
+      url: `https://api.nytimes.com/svc/books/v3/lists/2020-09-30/hardcover-fiction.json?api-key=${apiKey}`,
+      dataType: "json",
+      success: function(res){
+        console.log(res)
+      }
+      
+    }) 
+  }
+});
