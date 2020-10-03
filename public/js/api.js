8bbe55f68909b36de929d61682d1262c0a47628c
Enrique Lopez <elop22@gmail.com>
2020-10-03 13:34:00 -0600

    api calls functioning

--- public/js/api.js
+++ public/js/api.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
   $("#get-books").on("click", e => {
-    const search = $("#find-books").val();
+    const search = $("#search").val();
     findBooks(search);
     e.preventDefault();
   });
@@ -19,9 +19,9 @@ $(document).ready(() => {
           <div class="row">          
             <div class="card">
               <div class="card-image">
-                <img src="">
-                </div>
-                <div class="card-content">
+                <img src="${book.volumeInfo.imageLinks.thumbnail}">
+              </div>
+              <div class="card-content">
                 <span class="card-title">${book.volumeInfo.title}</span>
                 <h6>Written by ${book.volumeInfo.authors}</h6>
                 <p>${book.volumeInfo.description}</p>
@@ -34,6 +34,46 @@ $(document).ready(() => {
           `;
         });
         $("#google-books").html(output);
+      }
+    });
+  }
+  $("#get-nytlist").on("click", e => {
+    const find = $("#top-books").val();
+    topBooks(find);
+    e.preventDefault();
+  });
+  function topBooks() {
+    const apiKeyTwo = "uemMxG1ng0ii7KDe1ORTbiQTWqEVCDC8";
+    $.ajax({
+      method: "GET",
+      url: `https://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=${apiKeyTwo}`,
+      dataType: "json",
+      success: res => {
+        console.log(res);
+        const books = res.results.books;
+        let output = "";
+        $.each(books, (index, list) => {
+          console.log(list);
+          output += `
+          <div class="row">          
+            <div class="card">
+              <div class="card-image">
+                <img src="${list.book_image}">
+              </div>
+              <div class="card-content">
+                <span class="card-title">${list.title}</span>
+                <h6>Written by ${list.author}</h6>
+                <p>${list.description}</p>
+              </div>
+              <div class="card-action">
+                <a href="#">Add To Book List</a>
+                <a href="${list.amazon_product_url}"> Buy</a>
+              </div>
+            </div>           
+          </div> 
+          `;
+        });
+        $("#nytlist").html(output);
       }
     });
   }
