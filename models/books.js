6f626319246ab3d25660f8e844faf73e3df78cd3
hannahjorich <hannah.jorich@gmail.com>
2020-10-05 14:19:08 -0600

    added new model files for users and booklist

---
+++ models/books.js
@@ -0,0 +1,29 @@
+module.exports = function(sequelize, DataTypes) {
+    var Book = sequelize.define("Book", {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      body: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        len: [1]
+      }
+    });
+  
+    Book.associate = function(models) {
+      // We're saying that a book should belong to an user
+      // A Book can't be created without an user due to the foreign key constraint
+      Book.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
+    return Book;
+  };
+  
